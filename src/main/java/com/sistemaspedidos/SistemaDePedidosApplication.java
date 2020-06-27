bb2d04eb5dc7a3280cc1ae3a75112a34399bd8b9
Ariel Lopes <ariel.edit@gmail.com>
2020-06-27 17:51:00 -0400

    Criando operação de instanciação

--- src/main/java/com/sistemaspedidos/SistemaDePedidosApplication.java
+++ src/main/java/com/sistemaspedidos/SistemaDePedidosApplication.java
@@ -1,13 +1,33 @@
 package com.sistemaspedidos;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sistemaspedidos.domain.Categoria;
+import com.sistemaspedidos.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SistemaDePedidosApplication {
+public class SistemaDePedidosApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaDePedidosApplication.class, args);
+	}
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
