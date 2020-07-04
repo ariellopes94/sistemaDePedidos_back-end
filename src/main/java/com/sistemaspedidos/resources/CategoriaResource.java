556d24074351411d37cb5857880ffc92c6604eda
Ariel Lopes <ariel.edit@gmail.com>
2020-07-04 09:32:34 -0400

    criando INSERT na Categoria

--- src/main/java/com/sistemaspedidos/resources/CategoriaResource.java
+++ src/main/java/com/sistemaspedidos/resources/CategoriaResource.java
@@ -1,11 +1,16 @@
 package com.sistemaspedidos.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sistemaspedidos.domain.Categoria;
 import com.sistemaspedidos.services.CategoriaService;
@@ -22,6 +27,16 @@ public class CategoriaResource {
 	
        Categoria obj = categoriaService.buscarPorId(id);
 	   return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> insert(@RequestBody Categoria categoria){
+		Categoria obj = categoriaService.insert(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+				  buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/sistemaspedidos/services/CategoriaService.java
+++ src/main/java/com/sistemaspedidos/services/CategoriaService.java
@@ -1,8 +1,10 @@
 package com.sistemaspedidos.services;
 
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.sistemaspedidos.domain.Categoria;
 import com.sistemaspedidos.repositories.CategoriaRepository;
 import com.sistemaspedidos.services.exceptions.ObjectNotFoundException;
@@ -16,6 +18,12 @@ public class CategoriaService {
 	public Categoria buscarPorId(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Categorida não encontrado!"));		
+	}
+	
+	
+	public Categoria insert(Categoria categoria) {
+		  categoria.setId(null);
+		  return categoriaRepository.save(categoria);
 	}
 
 }
