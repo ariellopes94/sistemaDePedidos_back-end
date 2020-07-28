87116ea28671632fed267d44716e4abdd6059fe4
Ariel Lopes <ariel.edit@gmail.com>
2020-07-28 09:52:36 -0400

    Refresh TOKEN

---
+++ src/main/java/com/sistemaspedidos/resources/AuthResource.java
@@ -0,0 +1,31 @@
+package com.sistemaspedidos.resources;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sistemaspedidos.security.JWTUtil;
+import com.sistemaspedidos.security.UserSpringSecurity;
+import com.sistemaspedidos.services.UserService;
+
+
+@Resource
+@RequestMapping(value = "/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@PostMapping(value = "/refresh_token")
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSpringSecurity user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
+		return ResponseEntity.noContent().build();
+	}
+}
